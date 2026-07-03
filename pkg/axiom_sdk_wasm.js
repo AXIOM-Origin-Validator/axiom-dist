f216ae91ae7e98479f2ccdef4584290193a90812
AXIOM Origin <AXIOM-Origin-Validator@protonmail.com>
2026-07-03 23:45:38 +0900

    webclient 3.2.3 (CoreID a14e1327, html 0ded2f3febcdff6e32484061e0e0bc0db36ee08b82cbb9bf102de56c98b3cda7 / zip 488a1f1d95fa03911ccaba1d4a66e23a6edd8f426e59fa01b6ffd15bfb2a4107)

--- pkg/axiom_sdk_wasm.js
+++ pkg/axiom_sdk_wasm.js
@@ -1382,7 +1382,7 @@ function __wbg_get_imports() {
             console.warn(arg0);
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 253, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 254, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__he5c5569f8eeafad8);
             return ret;
         },
